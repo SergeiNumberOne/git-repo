0525b51653c965196db8296ff2e1a5f98a56390e
Sergei <brizgalov@gmail.com>
2017-02-10 14:20:49 +0300

    морской бассейн - текущий вид стр.

---
+++ basseyn-s-morskoy-vodoy.php
@@ -0,0 +1,177 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("keywords", "бассейн, морская вода, купание, фитнес-клуб");
+$APPLICATION->SetPageProperty("description", "Более подробную информацию о нашем бассейне на Севастопольской и других возможностях фитнес-клуба  «Кимберли-Лэнд» вы можете получить по телефону +7 (499) 372-30-99");
+$APPLICATION->SetTitle("Бассейн с морской водой");
+?> 
+<style>
+.columns .rightColumn {display: none !important;}
+.columns {padding: 0 15px 20px 15px !important;}
+</style>
+ 	
+<div class="row">
+  <div class="col-md-12 col-sm-12 col-xs-12">
+    <img class="img-responsive" src="/i/aqua/img_big_05.jpg" style="width: 100%; height: auto;" title="Бассейн с морской водой" alt="Бассейн с морской водой" >
+  </div>
+</div>
+
+<!-- <div class="col-sm-12 colorkids">
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <p>
+        <img class="img-responsive" src="/i/aqua/img_big_05.jpg" title="Бассейн с морской водой" alt="Бассейн с морской водой"  />
+      </p>
+    </div>
+
+   </div>
+ </div> -->
+ 
+<p data-select-like-a-boss="1"> 
+  <br />
+ </p>
+ 
+<div class="two-columns"> 
+  <div style="text-align: left;">Фитнес-клуб &laquo;Кимберли&raquo; по праву гордится своей аквазоной. Аквапарк с горками; бассейн для грудничков и бассейн для обучения плаванию; большой банный комплекс &ndash; все это доступно обладателям наших клубных карт. Но мы не останавливаемся на достигнутом: летом 2015 года в нашем клубе появился новый большой бассейн с морской водой.</div>
+ 
+  <p style="text-align: left;"><strong>Бассейн располагается на первом этаже новой двухэтажной зоны:</strong></p>
+ 
+  <ul> 
+    <li style="text-align: left;">длина бассейна – 25 метров;</li>
+   
+    <li style="text-align: left;">дорожек – 4;</li>
+   
+    <li style="text-align: left;">освещение – дневное и вечернее;</li>
+   
+    <li style="text-align: left;">специальная подсветка воды;</li>
+   
+    <li style="text-align: left;">подводная музыка.</li>
+   </ul>
+ 
+  <p style="text-align: left;"><strong>Дополнительное оборудование:</strong></p>
+ 
+  <ul> 
+    <li style="text-align: left;">пятиместная гидромассажная ванна;</li>
+   
+    <li style="text-align: left;">трехместная аэромассажная ванна;</li>
+   
+    <li style="text-align: left;">соляная баня из розовой гималайской соли;</li>
+   
+    <li style="text-align: left;">2 душа впечатлений;</li>
+   
+    <li style="text-align: left;">зона отдыха с шезлонгами.</li>
+   </ul>
+ 
+  <p style="text-align: left;"><strong>Второй этаж:</strong></p>
+ 
+  <ul> 
+    <li style="text-align: left;">массажная зона;</li>
+   
+    <li style="text-align: left;">сауна;</li>
+   
+    <li style="text-align: left;">зона отдыха с шезлонгами и качелями.</li>
+   
+    <li style="text-align: left;"> 
+      <br />
+     </li>
+   </ul>
+ 
+  <div class="widget-header" style="display: inline !important;"> 
+    <h3 class="widget-title" style="display: inline !important;"> </h3>
+   
+    <h4 style="text-align: left;">О морской воде</h4>
+   </div>
+ 
+  <p style="text-align: left;">Морская вода имеет сложный минеральный состав, который довольно близок к минеральному составу плазмы крови. Купание в такой воде делает кожу более упругой, а волосы - более мягкими и одновременно более сильными. Впрочем, стоит ли рассказывать: сегодня трудно найти человека, который не побывал бы на море хотя бы раз. Разница между купанием в реке и купанием в морской воде огромна. Морская вода приятнее и намного полезнее.</p>
+ 
+  <p style="text-align: left;">Столь же существенна разница меду бассейном с обычно хлорированной водой и бассейном с морской. Морская вода содержит большое количество ионов хлора и брома (а также некоторых других), которые обеспечивают эффективное обеззараживание. Именно поэтому такая вода практически не нуждается в дополнительном хлорировании или обеззараживании иным способом. </p>
+ 
+  <p> 
+    <br />
+   </p>
+ 
+  <div class="fitness" style="text-align: left;"> 
+    <div class="col-sm-12"> 
+      <div class="row"> 
+        <div class="col-md-4 col-xs-4 item"> 
+          <h4> 
+            <br />
+           </h4>
+         
+          <h4>Профилактика простудных заболеваний</h4>
+         
+          <p style="padding-top: 6px; padding-bottom: 6px;">Во время купания морская вода проникает глубоко в пазухи носа – даже в те места, которые остаются недоступными при полоскании носоглотки. Раствор солей губительно действует на болезнетворные организмы, колонии которых могут выживать на слизистой в течение многих лет. Именно поэтому плавание в морской воде является эффективным способом профилактики и отчасти лечения некоторых инфекционных заболеваний.</p>
+         
+          <p style="padding-top: 6px; padding-bottom: 6px;"> 
+            <br />
+           </p>
+         </div>
+       
+        <div class="col-md-4 col-xs-4 item"> 
+          <h4>Воздействие на нервную систему</h4>
+         
+          <p style="padding-top: 6px; padding-bottom: 6px;">Считается, что ионы брома, которые содержатся в морской воде, оказывают успокаивающее действие на нервную систему. Мы не знаем, подтвержден ли этот факт научно, но то, что плавание в морской воде успокаивает и позволяет расслабиться – несомненно.</p>
+         
+          <p style="padding-top: 6px; padding-bottom: 6px;"> 
+            <br />
+           </p>
+         </div>
+       
+        <div class="col-md-4 col-xs-4 item"> 
+          <h4>Большая плотность</h4>
+         
+          <p style="padding-top: 6px; padding-bottom: 6px;">Плотность морской воды несколько выше плотности обычной пресной. Поэтому в море или бассейне с такой водой легче плавать: вода сама удерживает тело на поверхности.</p>
+         </div>
+       </div>
+     
+      <div style="clear: left;"></div>
+     </div>
+   </div>
+ 
+  <div style="text-align: left; clear: left;"></div>
+ <hr style="text-align: left;" /> 
+  <p style="padding-top: 6px; padding-bottom: 6px; text-align: left;">Более подробную информацию о нашем бассейне на Севастопольской и других возможностях фитнес-клуба «Кимберли» вы можете получить по телефону клуба Кимберли</p>
+ 
+  <ul> </ul>
+ 
+  <br />
+ </div>
+ 
+<div class="two-columns"> 
+  <br />
+ </div>
+ 
+<div class="two-columns"> 
+  <br />
+ </div>
+ 
+<p data-select-like-a-boss="1"> 
+  <br />
+ </p>
+ 
+<div class="colorkids"> 	 
+  <div class="row" style="padding-top: 0px; background-image: url(&quot;/prenatal/img/pattern.png&quot;); background-color: rgb(43, 170, 225);"> 
+    <div class="col-md-9 col-xs-7"> 			 
+      <div style="margin-top: 0px;"> 				 
+        <h2 class="brand-col-white mt30">Подобрать клубную карту</h2>
+       			</div>
+     		</div>
+   
+    <div class="col-md-3 col-xs-3"> 
+      <div style="margin-top: 0px;"> 	 
+        <h2 class="mt30"><a class="btn btn-warning brendcolor_01 brand-col-white " href="/service/" role="button" >клубные карты</a></h2>
+       			</div>
+     		</div>
+   		 
+    <div style="clear: both; display: block; height: 15px;"></div>
+   	</div>
+ </div>
+ 
+<div style="clear: both; display: block; height: 10px;"></div>
+ 
+<div class="widget-header"> 	 
+  <h3 class="widget-title"><span style="font-family: AvenirNextCyrBold, sans-serif;">Фотографии:</span></h3>
+ </div>
+ 
+<div id="photos"> <iframe src="http://embedsocial.com/facebook_album/album_photos/903931516354021" width="100%" height="560" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
+ <link rel="stylesheet" href="/experiments/css/dk-landing-styles-atom.css"></link>
+ <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
